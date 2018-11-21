4521dcb8be3a02fb3a99ab2516d351fa246e918e
Sougata1233 <sougata.roy9203@gmail.com>
2018-11-21 16:41:03 +0530

    python data science second day

--- wordtokenization.py
+++ wordtokenization.py
@@ -1,6 +1,7 @@
 #Install NLTK before proceeding with the python program for word tokenization.
 #conda install -c anaconda nltk
 import nltk
+nltk.download('punkt')
 
 word_data = "It originated from the idea that there are readers who prefer learning new skills from the comforts of their drawing rooms"
 nltk_tokens = nltk.word_tokenize(word_data)
